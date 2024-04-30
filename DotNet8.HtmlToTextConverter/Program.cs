3d6460d69498dca77082ddd79784a462e0eb44a2
Zin Linn Htet <mackk5504@gmail.com>
2024-04-30 10:48:24 +0630

    Add project files.

---
+++ DotNet8.HtmlToTextConverter/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+
+string htmlContent = "<p>Hello <b>World!</b></p>";
+string plainText = ExtractTextFromHtml(htmlContent);
+Console.WriteLine(plainText); 
+string ExtractTextFromHtml(string html)
+{
+    if (html is null)
+    {
+        return "";
+    }
+
+    string plainText = Regex.Replace(html, "<[^>]+?>", " ");
+    plainText = System.Net.WebUtility.HtmlDecode(plainText).Trim();
+
+    return plainText;
+}
